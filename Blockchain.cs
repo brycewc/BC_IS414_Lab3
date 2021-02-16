f3cc5f038e90a12d914082bbc8b56692546bf38c
Bryce Cindrich <bryce.cindrich@gmail.com>
2021-02-16 15:32:54 -0800

    Add project files.

---
+++ Blockchain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace BC_IS414_Lab3
+{
+    class Blockchain
+    {
+        public List<Block> Chain { get; set; }
+        public int Difficulty { get; set; }
+        public List<Transaction> PendingTransactions { get; set; }
+        public decimal MiningReward { get; set; }
+        public Blockchain(int difficulty, decimal miningreward)
+        {
+            this.Chain = new List<Block>();
+            this.Chain.Add(CreateGenesisBlock());
+            this.Difficulty = difficulty;
+            this.MiningReward = miningreward;
+            this.PendingTransactions = new List<Transaction>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(0, DateTime.Now.ToString("yyyyMMddHHmmssffff"), new List<Transaction>());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return this.Chain.Last();
+        }
+
+        public void AddBlock(Block newBlock)
+        {
+            newBlock.PreviousHash = this.GetLatestBlock().Hash;
+            newBlock.Hash = newBlock.CalculateHash();
+            this.Chain.Add(newBlock);
+        }
+
+        public void AddPendingTransaction(Transaction transaction)
+        {
+            if (transaction.FromAddress is null || transaction.ToAddress is null)
+            {
+                throw new Exception("Transactions must include a from and to address");
+            }
+
+            if (transaction.Amount > this.GetBalanceOfWallet(transaction.FromAddress))
+            {
+                throw new Exception("Must be sufficent money in wallet");
+            }
+
+            if (transaction.IsValid() == false)
+            {
+                throw new Exception("Cannot add an invalid transaction to block");
+            }
+
+            this.PendingTransactions.Add(transaction);
+        }
+
+        public decimal GetBalanceOfWallet(PublicKey address)
+        {
+            decimal balance = 0;
+
+            string addressDER = BitConverter.ToString(address.toDer()).Replace("-", "");
+
+            foreach (Block block in this.Chain)
+            {
+                foreach (Transaction transaction in block.Transactions)
+                {
+                    if (!(transaction.FromAddress is null))
+                    {
+                        string fromDER = BitConverter.ToString(transaction.FromAddress.toDer()).Replace("-", "");
+                        
+
+                        if (fromDER == addressDER)
+                        {
+                            balance -= transaction.Amount;
+                        }
+                    }
+
+                    string toDER = BitConverter.ToString(transaction.ToAddress.toDer()).Replace("-", "");
+
+                    if (toDER == addressDER)
+                    {
+                        balance += transaction.Amount;
+                    }
+                }
+            }
+
+            return balance;
+        }
+
+        public void MinePendingTransactions(PublicKey miningRewardWallet)
+        {
+            Transaction rewardTx = new Transaction(null, miningRewardWallet, MiningReward);
+            this.PendingTransactions.Add(rewardTx);
+
+            Block newBlock = new Block(GetLatestBlock().Index + 1, DateTime.Now.ToString("yyyyMMddHHmmssffff"), this.PendingTransactions, GetLatestBlock().Hash);
+            newBlock.Mine(this.Difficulty);
+
+            Console.WriteLine("Block successfully mined!");
+            this.Chain.Add(newBlock);
+            this.PendingTransactions = new List<Transaction>();
+        }
+
+        public bool IsChainValid()
+        {
+            for (int i = 1; i < this.Chain.Count; i++)
+            {
+                Block currentBlock = this.Chain[i];
+                Block previousBlock = this.Chain[i - 1];
+
+                //Check if the current block hash is same as calculated hash
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.CalculateHash())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace BC_IS414_Lab3
+{
+    class Transaction
+    {
+        public PublicKey FromAddress { get; set; }
+        public PublicKey ToAddress { get; set; }
+        public decimal Amount { get; set; }
+        public Signature Signiture { get; set; }
+
+        public Transaction(PublicKey fromaddress, PublicKey toaddress, decimal amount)
+        {
+            this.FromAddress = fromaddress;
+            this.ToAddress = toaddress;
+            this.Amount = amount;
+        }
+
+        public void SignTransaction(PrivateKey signingkey)
+        {
+            string fromaddressDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string signingDER = BitConverter.ToString(signingkey.toDer()).Replace("-", "");
+
+            if(fromaddressDER != signingDER)
+            {
+                throw new Exception("You cannot sign transactions for other wallets");
+            }
+
+            string txHash = this.CalculateHash();
+            this.Signiture = Ecdsa.sign(txHash, signingkey);
+        }
+
+        public string CalculateHash()
+        {
+            string fromaddressDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string toaddressDER = BitConverter.ToString(ToAddress.toDer()).Replace("-", "");
+            string transactiondata = fromaddressDER + toaddressDER + Amount;
+            byte[] tdBytes = Encoding.ASCII.GetBytes(transactiondata);
+            return BitConverter.ToString(SHA256.Create().ComputeHash(tdBytes)).Replace("-", "");
+        }
+
+        public bool IsValid()
+        {
+            if (FromAddress is null) return true;
+
+            if(this.Signiture is null)
+            {
+                throw new Exception("No signiture in this transaction");
+            }
+
+            return Ecdsa.verify(this.CalculateHash(), this.Signiture, this.FromAddress);
+        }
+    }
+}
